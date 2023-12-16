4fc8f7a5b2ed41dce8b576d4950dd7be9f2d8eb4
Jenorg <jenfermz44@gmail.com>
2023-12-16 14:21:00 -0400

    create chronometer

---
+++ app.js
@@ -0,0 +1,45 @@
+const playButton = document.getElementById('play-pause');
+const resetButton = document.getElementById('reset');
+const clock = document.getElementById('clock');
+
+let active = false;
+let intervale;
+let elapse = 0;
+
+function secondsToTime(seconds) {
+    const hours = Math.floor(seconds / (60*60));
+    const minutes = Math.floor((seconds % (60*60) / 60));
+    const remainingSeconds = seconds % 60;
+    
+    return `${addZero(hours)}:${addZero(minutes)}:${addZero(remainingSeconds)}`;
+}
+
+function addZero(number) {
+    return number < 10 ? '0'+number : number;
+}
+
+function updateClock() {
+    elapse += 1;
+    clock.innerText = secondsToTime(elapse);
+}
+
+playButton.addEventListener('click', ()=>{    
+    if(!active) {
+        active = true;
+        playButton.innerHTML = '<i class="fa-solid fa-pause"></i>';               
+        intervale = window.setInterval(updateClock, 1000)
+    } else {
+        active = false;
+        actualTime = clock.innerText;
+        playButton.innerHTML = '<i class="fa-solid fa-play"></i>'; 
+        window.clearInterval(intervale);        
+    }
+})
+
+resetButton.addEventListener('click', ()=>{
+    clock.innerText = '00:00:00';
+    playButton.innerHTML = '<i class="fa-solid fa-play"></i>'
+    window.clearInterval(intervale);
+    active = false;
+    elapse = 0;
+})
